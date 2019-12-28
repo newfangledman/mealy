38774d37b30cfd9524a09fb09765b6b3fdebb7f9
adam <adam@harpur.io>
2019-12-28 13:47:34 +0000

    chore: setup es6 import/export

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
-
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-var root = { hello: () => 'Hello world!' };
-
-var app = express();
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
-app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
+// Transpile all code following this line with babel and use '@babel/preset-env' (aka ES6) preset.
+require("@babel/register")({
+    presets: ["@babel/preset-env"]
+  });
+  
+  // Import the rest of our application.
+  module.exports = require('./server.js')
+  
---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+var schema = buildSchema(`
+  type Query {
+    hello: String,
+    test: String
+  }
+`);
+
+var root = { hello: () => 'Hello world!', test: () => 'this is a test' };
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
