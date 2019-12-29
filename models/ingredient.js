6e27384e4aaf7fcc65682bf70791a10559e0f3d7
adam <adam@harpur.io>
2019-12-29 14:45:52 +0000

    feat: setup schema

--- models/ingredient.js
+++ models/ingredient.js
@@ -8,7 +8,8 @@ export default (sequelize, DataTypes) => {
         autoIncrement: true,
         primaryKey: true
       },
-      name: DataTypes.STRING
+      name: DataTypes.STRING,
+      description: DataTypes.STRING
     },
     { underscored: true }
   );
--- schema/index.js
+++ schema/index.js
@@ -1,1 +1,55 @@
-// schemas
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString
+} from "graphql";
+
+const mealType = new GraphQLObjectType({
+  name: "Meal",
+  fields: {
+    name: {
+      type: GraphQLString
+    },
+    description: {
+      type: GraphQLString
+    }
+  }
+});
+
+const ingredientType = new GraphQLObjectType({
+  name: "Ingredient",
+  fields: {
+    name: {
+      type: GraphQLString
+    },
+    description: {
+      type: GraphQLString
+    }
+  }
+});
+
+const queryType = new GraphQLObjectType({
+  name: "Query",
+  fields: {
+    meal: {
+      type: mealType,
+      args: {
+        id: { type: GraphQLInt }
+      },
+      resolve: (source, { id }) => {
+        return "meal";
+      }
+    },
+    meals: {
+      type: new GraphQLList(mealType),
+      resolve: () => {
+        return "meals";
+      }
+    }
+  }
+});
+
+export default new GraphQLSchema({
+  query: queryType
+});
